f6d91ec12112899e6a40661643e308732c156b73
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-07 18:00:37 +0100

    Primeros cambios realizados en el código

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,5 +1,9 @@
 package demo;
 
 public class Demo {
-
+	public static void main(String[] args) {
+		System.out.println("linea 1");
+		System.out.println("linea 1");
+		System.out.println("linea 1");
+	}
 }
