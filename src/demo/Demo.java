b45cf85aa3f988206f15d278ece1dca8a101de46
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-07 18:16:23 +0100

    cambio y borrado

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,7 +3,7 @@ package demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-		System.out.println("linea 1");
-		System.out.println("linea 1");
+		System.out.println("linea 2");
+		System.out.println("linea 3");
 	}
 }
