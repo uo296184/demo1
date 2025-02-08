362ba3748eb0c839555a50de534361fd53d1c30f
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-08 13:11:50 +0100

    cambio 2 en master

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
 		System.out.println("cambio 1 master");
 		System.out.println("linea 3");
 		System.out.println("linea 4");
-		System.out.println("linea 5");
+		System.out.println("cambio 2 master");
 		
 	}
 }
