1d4ef474fbd8dd6e65fce628d7142820f2a9b031
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-08 13:53:42 +0100

    cambio 2 en rebase

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("cambio 1 en rebase");
 		System.out.println("linea 2");
-		System.out.println("cambio 2 en master");
+		System.out.println("linea 3");
 		System.out.println("cambio 2 en rebase");
 	}
 }
