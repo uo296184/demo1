3cbfc37a2a395dfcbf13ada31f0d654778ae58d6
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-08 11:52:17 +0100

    Cambio en la rama 2

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,8 +4,8 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
 		System.out.println("linea 2");
-		System.out.println("linea 3");
+		System.out.println("Cambio en la rama cambio2");
 		System.out.println("linea 4");
-		System.out.println("linea 5");
+		System.out.println("liena 5");
 	}
 }
