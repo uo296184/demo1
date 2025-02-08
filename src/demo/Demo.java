1c967b068f2eb10ac9600fab628a07bbf6d8d85f
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-08 13:16:00 +0100

    reinicio cambio-rebase

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,8 +4,8 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
 		System.out.println("linea 2");
-		System.out.println("cambio 1 cambio-rebase");
-		System.out.println("cambio 2 cambio-rebase");
+		System.out.println("linea 3");
+		System.out.println("linea 4");
 		System.out.println("linea 5");
 		
 	}
