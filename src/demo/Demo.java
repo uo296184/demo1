069c2ecc527f78f7b2d2054e1575e1b49c70999e
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-08 13:45:00 +0100

    cambio 1 en rebase

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -2,7 +2,7 @@ package demo;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("linea 1");
+		System.out.println("cambio 1 en rebase");
 		System.out.println("linea 2");
 		System.out.println("linea 3");
 		System.out.println("linea 4");
