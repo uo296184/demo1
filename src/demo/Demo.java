b2962bd7cc0a8bf7663c84f88759ded3fa1c8c3e
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-08 12:08:19 +0100

    vuelta al original

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,8 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
 		System.out.println("linea 2");
-		System.out.println("Cambio en la rama cambio2");
-		System.out.println("cambio en el master");
-		System.out.println("liena 5");
+		System.out.println("linea 3");
 	}
 }
