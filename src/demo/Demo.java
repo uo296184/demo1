feeb79173cc81018ee54ae79ce5e5477c79ac169
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-09 11:23:34 +0100

    cambio 2 rebase

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("cambio 1 master");
 		System.out.println("linea 2");
-		System.out.println("linea 3");
+		System.out.println("cambio 2 master");
 		System.out.println("linea 4");
 	}
 }
