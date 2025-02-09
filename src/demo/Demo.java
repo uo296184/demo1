ad49ddea45ba874e4e18aad36d4a479944eeeb99
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-09 11:14:02 +0100

    reinicio del estado de Demo

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -2,9 +2,9 @@ package demo;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("cambio 1 en rebase");
-		System.out.println("cambio 1 en master");
-		System.out.println("cambio 2 en master");
-		System.out.println("cambio 2 en rebase");
+		System.out.println("linea 1");
+		System.out.println("linea 2");
+		System.out.println("linea 3");
+		System.out.println("linea 4");
 	}
 }
