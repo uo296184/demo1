3d66db53d83aaafde27ff85dbc67f6f25a3bc884
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-09 12:05:09 +0100

    cambio 2 master

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("cambio 1");
 		System.out.println("linea 2");
-		System.out.println("linea 3");
+		System.out.println("cambio 3");
 		System.out.println("linea 4");
 	}
 }
