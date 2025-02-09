44e79d4423fa57d9cc4c19980e7a5adf11ea78ad
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-09 11:47:16 +0100

    reinicio código

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -2,9 +2,9 @@ package demo;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("cambio 1 master");
+		System.out.println("linea 1");
 		System.out.println("linea 2");
-		System.out.println("cambio 2 master");
+		System.out.println("linea 3");
 		System.out.println("linea 4");
 	}
 }
