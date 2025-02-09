5cef412b54135df79c925761f73626c75a598f70
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-09 12:44:12 +0100

    reinicio condigo

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,8 +3,8 @@ package demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-		System.out.println("cambio en master");
+		System.out.println("linea 2");
 		System.out.println("linea 3");
-		System.out.println("cambio en rebase");
+		System.out.println("linea 4");
 	}
 }
