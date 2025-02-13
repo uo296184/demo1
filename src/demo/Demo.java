6e130f849135bc26ed898366faf837988e87a2b7
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-13 23:52:15 +0100

    cambio 1 rama

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,7 +3,7 @@ package demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-		System.out.println("linea 2");
+		System.out.println("cambio 1 rama");
 		System.out.println("cambio 1 master");
 		System.out.println("linea 4");
 		System.out.println("cambio 2 master");
