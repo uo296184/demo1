42e6f1cbbff8c0b8ac14d31ff00a193a758cf494
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-13 23:53:47 +0100

    cambio 2 rama

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 		System.out.println("linea 1");
 		System.out.println("cambio 1 rama");
 		System.out.println("cambio 1 master");
-		System.out.println("linea 4");
+		System.out.println("cambio 2 rama");
 		System.out.println("cambio 2 master");
 	}
 }
