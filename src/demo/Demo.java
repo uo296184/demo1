33e38eff0cc71ac621311ab254b4af4d03586d24
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-14 00:51:54 +0100

    reinicio codigo

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,9 +3,9 @@ package demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-		System.out.println("cambio en rama sin conflictos");
+		System.out.println("linea 2");
 		System.out.println("linea 3");
-		System.out.println("cambio en master");
+		System.out.println("linea 4");
 		System.out.println("linea 5");
 	}
 }
