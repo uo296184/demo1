ffca41bdc65646532056e1ef2e6fbe3f8e4cff7d
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-13 23:13:00 +0100

    Reinicio código

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,9 +3,9 @@ package demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-		System.out.println("modificación en master");
+		System.out.println("linea 2");
 		System.out.println("linea 3");
-		System.out.println("modificación sin conflictos");
+		System.out.println("linea 4");
 		System.out.println("linea 5");
 	}
 }
