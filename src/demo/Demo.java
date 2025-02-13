3d969af620c55facce4d6d6819581e167787212d
Hugo Fáez Jorquera <Hugo Fez Jorquera@192.168.1.157>
2025-02-13 23:34:53 +0100

    reinicio codigo

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
 		System.out.println("linea 2");
-		System.out.println("cambio 2");
+		System.out.println("linea 3");
 		System.out.println("linea 4");
 		System.out.println("linea 5");
 	}
